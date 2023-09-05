1cad3a4bf7376b5d68e4b0ae857c97b78296ddcc
Cristofferb7 <136852733+Cristofferb7@users.noreply.github.com>
2023-09-05 17:20:24 -0400

    added changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,8 +9,8 @@ function Projects(props) {
         <div class="col">
           <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"  id="image-2">
             <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-              <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
-              </h3>
+              <a href="https://github.com/crandonbruz/scoreboard-analytics" class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold " >
+                 </a>
               <ul class="d-flex list-unstyled mt-auto">
                 <li class="me-auto">
                 </li> 
@@ -22,8 +22,8 @@ function Projects(props) {
         <div class="col">
           <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"  id="image-3">
             <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-              <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
-              </h3>
+            <a href="https://github.com/WyattBensman/the-merry-wishlist" class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold " >
+                 </a>
               <ul class="d-flex list-unstyled mt-auto">
                 <li class="me-auto">
                 </li> 
@@ -35,30 +35,21 @@ function Projects(props) {
         <div class="col">
           <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"  id="image-4">
             <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-              <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold hover-1">
-              hello world
-              </h3>
+            <a href="https://github.com/crandonbruz/scoreboard-analytics" class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold " >
+                 </a>
               <ul class="d-flex list-unstyled mt-auto">
                 <li class="me-auto">
-                  <img
-                    src="https://github.com/twbs.png"
-                    alt="Bootstrap"
-                    width="32"
-                    height="32"
-                    class="rounded-circle border border-white"
-                  />
                 </li>
                 <li class="d-flex align-items-center me-3">
                   <svg class="bi me-2" width="1em" height="1em">
                     <use xlink:href="#geo-fill"></use>
                   </svg>
-                  <small>California</small>
+               
                 </li>
                 <li class="d-flex align-items-center">
                   <svg class="bi me-2" width="1em" height="1em">
                     <use xlink:href="#calendar3"></use>
                   </svg>
-                  <small>5d</small>
                 </li>
               </ul>
             </div>
@@ -69,30 +60,21 @@ function Projects(props) {
       <div class="col">
           <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"  id="image-5">
             <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-              <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
-                Another longer title belongs here
-              </h3>
+            <a href="https://github.com/Cristofferb7/weather-app" class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold " >
+                 </a>
               <ul class="d-flex list-unstyled mt-auto">
                 <li class="me-auto">
-                  <img
-                    src="https://github.com/twbs.png"
-                    alt="Bootstrap"
-                    width="32"
-                    height="32"
-                    class="rounded-circle border border-white"
-                  />
                 </li>
                 <li class="d-flex align-items-center me-3">
                   <svg class="bi me-2" width="1em" height="1em">
                     <use xlink:href="#geo-fill"></use>
                   </svg>
-                  <small>California</small>
                 </li>
                 <li class="d-flex align-items-center">
                   <svg class="bi me-2" width="1em" height="1em">
                     <use xlink:href="#calendar3"></use>
                   </svg>
-                  <small>5d</small>
+              
                 </li>
               </ul>
             </div>
@@ -104,30 +86,21 @@ function Projects(props) {
         <div class="col">
           <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"  id="image-5">
             <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-              <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
-                Another longer title belongs here
-              </h3>
+            <a href="https://github.com/Cristofferb7/workday-schedule" class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold " >
+                 </a>
               <ul class="d-flex list-unstyled mt-auto">
-                <li class="me-auto">
-                  <img
-                    src="https://github.com/twbs.png"
-                    alt="Bootstrap"
-                    width="32"
-                    height="32"
-                    class="rounded-circle border border-white"
-                  />
-                </li>
+
                 <li class="d-flex align-items-center me-3">
                   <svg class="bi me-2" width="1em" height="1em">
                     <use xlink:href="#geo-fill"></use>
                   </svg>
-                  <small>California</small>
+                 
                 </li>
                 <li class="d-flex align-items-center">
                   <svg class="bi me-2" width="1em" height="1em">
                     <use xlink:href="#calendar3"></use>
                   </svg>
-                  <small>5d</small>
+                
                 </li>
               </ul>
             </div>
@@ -138,30 +111,21 @@ function Projects(props) {
         <div class="col">
           <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"  id="image-3">
             <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-              <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
-                Another longer title belongs here
-              </h3>
+            <a href="https://github.com/crandonbruz/scoreboard-analytics" class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold " >
+                 </a>
               <ul class="d-flex list-unstyled mt-auto">
                 <li class="me-auto">
-                  <img
-                    src="https://github.com/twbs.png"
-                    alt="Bootstrap"
-                    width="32"
-                    height="32"
-                    class="rounded-circle border border-white"
-                  />
                 </li>
                 <li class="d-flex align-items-center me-3">
                   <svg class="bi me-2" width="1em" height="1em">
                     <use xlink:href="#geo-fill"></use>
                   </svg>
-                  <small>California</small>
+                
                 </li>
                 <li class="d-flex align-items-center">
                   <svg class="bi me-2" width="1em" height="1em">
                     <use xlink:href="#calendar3"></use>
                   </svg>
-                  <small>5d</small>
                 </li>
               </ul>
             </div>
